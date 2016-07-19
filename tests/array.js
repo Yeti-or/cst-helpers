0152732cf6fe01ca8813238e18cc07cd6d3229e8
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2016-07-19 17:32:34 +0300

    Imrove arr tests

--- tests/array.js
+++ tests/array.js
@@ -98,73 +98,167 @@ describe('Array:', () => {
     });
 
     describe('remove', () => {
-        it('should remove from array [x]', () => {
-            var one = new types.NumericLiteral([Token.create('Numeric', 1)]);
-            var arr = helpers.createArray([one]);
-            helpers.removeElementFromArray(arr, one);
-            expect(arr.getSourceCode()).to.eql('[]');
-
-            arr = helpers.createArray([(one = one.cloneElement())], {multiLine: true});
-            helpers.removeElementFromArray(arr, one);
-            expect(arr.getSourceCode()).to.eql('[]');
+        beforeEach(() => {
+            this.createArr = (opts) => {
+                var arr = [
+                    new types.NumericLiteral([new Token('Numeric', 1)]),
+                    new types.NumericLiteral([new Token('Numeric', 2)]),
+                    new types.NumericLiteral([new Token('Numeric', 3)])
+                ];
+                return helpers.createArray(arr, opts);
+            };
         });
 
         it('should remove from array [1, x, 3]', () => {
-            var one = new types.NumericLiteral([new Token('Numeric', 1)]);
-            var two = new types.NumericLiteral([new Token('Numeric', 2)]);
-            var three = new types.StringLiteral([new Token('String', '"three"')]);
-            var arr = helpers.createArray([one, two, three]);
+            var arr = this.createArr();
+            helpers.removeElementFromArray(arr, arr.elements[1]);
+            expect(arr.getSourceCode()).to.eql('[1, 3]');
 
-            helpers.removeElementFromArray(arr, two);
-            // expect(arr.getSourceCode()).to.eql('[1,"three"]');
+            arr = this.createArr({spacesInsideArrayBrackets: true});
+            helpers.removeElementFromArray(arr, arr.elements[1]);
+            expect(arr.getSourceCode()).to.eql('[ 1, 3 ]');
 
-            var arr2 = helpers.createArray([one.cloneElement(), (two = two.cloneElement()), three.cloneElement()]);
+            arr = this.createArr({newlineAfterArrayElements: true});
+            helpers.removeElementFromArray(arr, arr.elements[1]);
+            expect(arr.getSourceCode()).to.eql('[1,\n3]');
 
-            helpers.removeElementFromArray(arr2, two);
-            expect(arr2.getSourceCode()).to.eql('[1, "three"]');
+            arr = this.createArr({paddingNewLinesInArray: true});
+            helpers.removeElementFromArray(arr, arr.elements[1]);
+            expect(arr.getSourceCode()).to.eql('[\n1, 3\n]');
+
+            arr = this.createArr({multiLine: true});
+            helpers.removeElementFromArray(arr, arr.elements[1]);
+            expect(arr.getSourceCode()).to.eql('[\n1,\n3\n]');
+        });
+
+        it('should remove from array [x, 2, 3]', () => {
+            var arr = this.createArr();
+            helpers.removeElementFromArray(arr, arr.elements[0]);
+            expect(arr.getSourceCode()).to.eql('[2, 3]');
+
+            arr = this.createArr({spacesInsideArrayBrackets: true});
+            helpers.removeElementFromArray(arr, arr.elements[0]);
+            expect(arr.getSourceCode()).to.eql('[ 2, 3 ]');
+
+            arr = this.createArr({newlineAfterArrayElements: true});
+            helpers.removeElementFromArray(arr, arr.elements[0]);
+            expect(arr.getSourceCode()).to.eql('[2,\n3]');
+
+            arr = this.createArr({paddingNewLinesInArray: true});
+            helpers.removeElementFromArray(arr, arr.elements[0]);
+            expect(arr.getSourceCode()).to.eql('[\n2, 3\n]');
+
+            arr = this.createArr({multiLine: true});
+            helpers.removeElementFromArray(arr, arr.elements[0]);
+            expect(arr.getSourceCode()).to.eql('[\n2,\n3\n]');
+        });
+
+        it('should remove from array [1, 2, x]', () => {
+            var arr = this.createArr();
+            helpers.removeElementFromArray(arr, arr.elements[2]);
+            expect(arr.getSourceCode()).to.eql('[1, 2]');
+
+            arr = this.createArr({spacesInsideArrayBrackets: true});
+            helpers.removeElementFromArray(arr, arr.elements[2]);
+            expect(arr.getSourceCode()).to.eql('[ 1, 2 ]');
+
+            arr = this.createArr({newlineAfterArrayElements: true});
+            helpers.removeElementFromArray(arr, arr.elements[2]);
+            expect(arr.getSourceCode()).to.eql('[1,\n2]');
+
+            arr = this.createArr({paddingNewLinesInArray: true});
+            helpers.removeElementFromArray(arr, arr.elements[2]);
+            expect(arr.getSourceCode()).to.eql('[\n1, 2\n]');
+
+            arr = this.createArr({multiLine: true});
+            helpers.removeElementFromArray(arr, arr.elements[2]);
+            expect(arr.getSourceCode()).to.eql('[\n1,\n2\n]');
         });
 
         it('should remove from array [1, x]', () => {
-            var one = new types.NumericLiteral([new Token('Numeric', 1)]);
-            var two = new types.NumericLiteral([new Token('Numeric', 2)]);
-            var arr = helpers.createArray([one, two]);
-
-            helpers.removeElementFromArray(arr, two);
+            var arr = this.createArr();
+            helpers.removeElementFromArray(arr, arr.elements[2]);
+            helpers.removeElementFromArray(arr, arr.elements[1]);
             expect(arr.getSourceCode()).to.eql('[1]');
 
-            var arr2 = helpers.createArray([one.cloneElement(), (two = two.cloneElement())]);
+            arr = this.createArr({spacesInsideArrayBrackets: true});
+            helpers.removeElementFromArray(arr, arr.elements[2]);
+            helpers.removeElementFromArray(arr, arr.elements[1]);
+            expect(arr.getSourceCode()).to.eql('[ 1 ]');
 
-            helpers.removeElementFromArray(arr2, two);
-            expect(arr2.getSourceCode()).to.eql('[1]');
+            arr = this.createArr({newlineAfterArrayElements: true});
+            helpers.removeElementFromArray(arr, arr.elements[2]);
+            helpers.removeElementFromArray(arr, arr.elements[1]);
+            expect(arr.getSourceCode()).to.eql('[1]');
+
+            arr = this.createArr({paddingNewLinesInArray: true});
+            helpers.removeElementFromArray(arr, arr.elements[2]);
+            helpers.removeElementFromArray(arr, arr.elements[1]);
+            expect(arr.getSourceCode()).to.eql('[\n1\n]');
+
+            arr = this.createArr({multiLine: true});
+            helpers.removeElementFromArray(arr, arr.elements[2]);
+            helpers.removeElementFromArray(arr, arr.elements[1]);
+            expect(arr.getSourceCode()).to.eql('[\n1\n]');
         });
 
-        it('should remove from array [x, 1, 2]', () => {
-            var one = new types.NumericLiteral([new Token('Numeric', 1)]);
-            var two = new types.NumericLiteral([new Token('Numeric', 2)]);
-            var three = new types.StringLiteral([new Token('String', '"three"')]);
-            var arr = helpers.createArray([one, two, three]);
-
-            helpers.removeElementFromArray(arr, one);
-            // expect(arr.getSourceCode()).to.eql('[2,"three"]');
-
-            var arr2 = helpers.createArray([(one = one.cloneElement()), two.cloneElement(), three.cloneElement()]);
-
-            helpers.removeElementFromArray(arr2, one);
-            expect(arr2.getSourceCode()).to.eql('[2, "three"]');
-        });
-
-        it('should remove from array [x, 1]', () => {
-            var one = new types.NumericLiteral([new Token('Numeric', 1)]);
-            var two = new types.NumericLiteral([new Token('Numeric', 2)]);
-            var arr = helpers.createArray([one, two]);
-
-            helpers.removeElementFromArray(arr, one);
+        it('should remove from array [x, 2]', () => {
+            var arr = this.createArr();
+            helpers.removeElementFromArray(arr, arr.elements[2]);
+            helpers.removeElementFromArray(arr, arr.elements[0]);
             expect(arr.getSourceCode()).to.eql('[2]');
 
-            var arr2 = helpers.createArray([(one = one.cloneElement()), two.cloneElement()]);
+            arr = this.createArr({spacesInsideArrayBrackets: true});
+            helpers.removeElementFromArray(arr, arr.elements[2]);
+            helpers.removeElementFromArray(arr, arr.elements[0]);
+            expect(arr.getSourceCode()).to.eql('[ 2 ]');
 
-            helpers.removeElementFromArray(arr2, one);
-            expect(arr2.getSourceCode()).to.eql('[2]');
+            arr = this.createArr({newlineAfterArrayElements: true});
+            helpers.removeElementFromArray(arr, arr.elements[2]);
+            helpers.removeElementFromArray(arr, arr.elements[0]);
+            expect(arr.getSourceCode()).to.eql('[2]');
+
+            arr = this.createArr({paddingNewLinesInArray: true});
+            helpers.removeElementFromArray(arr, arr.elements[2]);
+            helpers.removeElementFromArray(arr, arr.elements[0]);
+            expect(arr.getSourceCode()).to.eql('[\n2\n]');
+
+            arr = this.createArr({multiLine: true});
+            helpers.removeElementFromArray(arr, arr.elements[2]);
+            helpers.removeElementFromArray(arr, arr.elements[0]);
+            expect(arr.getSourceCode()).to.eql('[\n2\n]');
+        });
+
+        it('should remove from array [x]', () => {
+            var arr = this.createArr();
+            helpers.removeElementFromArray(arr, arr.elements[2]);
+            helpers.removeElementFromArray(arr, arr.elements[1]);
+            helpers.removeElementFromArray(arr, arr.elements[0]);
+            expect(arr.getSourceCode()).to.eql('[]');
+
+            arr = this.createArr({spacesInsideArrayBrackets: true});
+            helpers.removeElementFromArray(arr, arr.elements[2]);
+            helpers.removeElementFromArray(arr, arr.elements[1]);
+            helpers.removeElementFromArray(arr, arr.elements[0]);
+            expect(arr.getSourceCode()).to.eql('[]');
+
+            arr = this.createArr({newlineAfterArrayElements: true});
+            helpers.removeElementFromArray(arr, arr.elements[2]);
+            helpers.removeElementFromArray(arr, arr.elements[1]);
+            helpers.removeElementFromArray(arr, arr.elements[0]);
+            expect(arr.getSourceCode()).to.eql('[]');
+
+            arr = this.createArr({paddingNewLinesInArray: true});
+            helpers.removeElementFromArray(arr, arr.elements[2]);
+            helpers.removeElementFromArray(arr, arr.elements[1]);
+            helpers.removeElementFromArray(arr, arr.elements[0]);
+            expect(arr.getSourceCode()).to.eql('[]');
+
+            arr = this.createArr({multiLine: true});
+            helpers.removeElementFromArray(arr, arr.elements[2]);
+            helpers.removeElementFromArray(arr, arr.elements[1]);
+            helpers.removeElementFromArray(arr, arr.elements[0]);
+            expect(arr.getSourceCode()).to.eql('[]');
         });
 
     });
